edef0e722eaff8785d61b78051c2ba304897b7c0
Pedro Gomes <pedrogomes94@gmail.com>
2018-12-21 19:20:33 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-styled-typescript-boilerplate
+
+Built with Create-React-App using React-Scripts-TS and Styled-Components
---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import * as React from "react";
+import styled, { keyframes } from "styled-components";
+import logo from "./assets/logo.svg";
+import { colors, fonts } from "./styles";
+
+const logoSpin = keyframes`
+  from { transform: rotate(0deg); }
+  to { transform: rotate(360deg); }
+`;
+
+const SApp = styled.div`
+  text-align: center;
+`;
+
+const SAppHeader = styled.header`
+  background-color: rgb(${colors.grey});
+  padding: 10px;
+  color: rgb(${colors.white});
+`;
+
+const SAppLogo = styled.img`
+  animation: ${logoSpin} infinite 20s linear;
+  height: 100px;
+`;
+
+const SAppTitle = styled.h1`
+  margin: 20px auto;
+`;
+
+const SAppIntro = styled.p`
+  margin: 40px auto;
+  font-size: ${fonts.size.large};
+`;
+
+class App extends React.Component {
+  public render() {
+    return (
+      <SApp>
+        <SAppHeader>
+          <SAppLogo src={logo} alt="logo" />
+          <SAppTitle>Welcome to React</SAppTitle>
+        </SAppHeader>
+        <SAppIntro>
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </SAppIntro>
+      </SApp>
+    );
+  }
+}
+
+export default App;
